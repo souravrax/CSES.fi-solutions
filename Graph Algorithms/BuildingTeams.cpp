990e831d9ed865c0047774d4a03bef60041f7f04
Sourav Rakshit <rakshitsourav3@gmail.com>
2020-09-20 16:27:27 +0530

    Solved Building Team [m-coloring]

---
+++ Graph Algorithms/BuildingTeams.cpp
@@ -0,0 +1,132 @@
+/**
+ *  author: sourav rakshit
+ *  created: 20.09.2020 16:00:10
+**/
+
+/* #pragma GCC optimize("O3") */
+/* #pragma GCC target ("avx2") */
+/* #pragma GCC optimize("unroll-loops") */
+/* #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native") */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+//@alias
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+//@Macros
+#define ar array
+#define len(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define str(s) to_string(s)
+
+//@Magic Lambdas
+namespace std {
+template<class Fun> class rec_result{
+Fun fun_;
+public:
+template<class T>
+explicit rec_result(T &&fun): fun_(std::forward<T>(fun)) {}
+template<class ...Args>
+decltype(auto) operator()(Args &&...args) {
+return fun_(std::ref(*this), std::forward<Args>(args)...);}};
+template<class Fun>decltype(auto) rec(Fun &&fun) {
+return rec_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}} // namespace std
+
+//@Debugger
+#ifdef LOCAL
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v)
+{ os << '{'; string sep; for (const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename T, size_t size> ostream& operator<<(ostream &os, const array<T, size> &arr) 
+{ os << '{'; string sep; for (const auto &x : arr) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) 
+{ return os << '(' << p.first << "," << p.second << ')'; }
+
+void flush_out() { cerr << endl; }
+template<typename H, typename... T> void flush_out(H h, T... t) { cerr << ' ' << h << " | "; flush_out(t...); }
+#define log(...) cerr << "[ " << #__VA_ARGS__ << " ] :", flush_out(__VA_ARGS__)
+#else
+#define log(...) (void)0x30
+#endif
+
+//@Utilities
+template<typename T> inline void mini(T& a, T b) { a = min(a, b); }
+template<typename T> inline void maxi(T& a, T b) { a = max(a, b); }
+template<class T>class Random{ static T range(T&a, T&b){ return a+rand()%(b-a+1); }};
+
+//*//---------------------------- Globals --------------------------//*//
+
+const int mod = 1e9 + 7;
+const ll INF = 1e18;
+const int nax = 2000 * 2007;
+const ld PI = 4*atan((ld)1);
+
+//*//---------------------------- Code Begins Here --------------------------//*//
+
+vector<int> colors;
+vector<bool> visited;
+
+void f(const vector<vector<int>>& G, int node, int color) {
+    visited[node] = true;
+    colors[node] = color + 1;
+    for(auto& i : G[node]) {
+        if(visited[i] && colors[i] != (!color) + 1) {
+            cout << "IMPOSSIBLE\n";
+            exit(0);
+        }
+        if(!visited[i]) f(G, i, !color);
+    }
+}
+
+inline void tc() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> G(n + 1);
+    while(m--) { int a, b; cin >> a >> b; G[a].push_back(b); G[b].push_back(a); }
+    visited.resize(n + 1, false);
+    colors.resize(n + 1);
+    for(int i = 1; i <= n; i++) {
+        if(!visited[i]) f(G, i, 0);
+    }
+    log(colors);
+    if(count(all(colors), 1) == n) {
+        for(int i = 0; i < n - 1; i++) cout << 1 << ' ';
+        cout << 2 << '\n';
+        exit(0);
+    }
+    for(int i = 1; i <= n; i++) {
+        cout << colors[i] << " \n"[i == n];
+    }
+    colors.clear();
+    visited.clear();
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+#ifndef LOCAL
+    cin.tie(nullptr);
+#endif
+    int tt = 1;
+    for(int _=tt-1;~_;--_){
+        tc();
+    }
+#ifdef LOCAL
+    cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s.\n";
+#endif
+}
+
+/* ============ 😇 NEED HELP? ============ */ 
+/* Take a look the the overflows */
+/* IndexOutOfBound? Accessing nullptr? */
+/* What about some stress testing? */
+/* Flood with log or use GDB */
+/* ============ NOW SOLVE 👊 ============ */ 
+
