8265a4f9a6ba4b546e03b0d493ab84bd4cf8787e
Sourav Rakshit <rakshitsourav3@gmail.com>
2021-04-24 09:37:02 +0530

    Solved Removal Game

--- Dynamic Programming/RemovalGame.cpp
+++ Dynamic Programming/RemovalGame.cpp
@@ -14,13 +14,21 @@ int32_t main(int argc, char* argv[]) {
     }
 
     vector<vector<ll>> dp(n, vector<ll>(n));
-    for(int i=n-1; ~i; i--) {
-        for(int j = i; j<n; j++) {
+    /* for(int i=n-1; ~i; i--) { */
+    /*     for(int j = i; j<n; j++) { */
+    /*         if(i == j) dp[i][j] = arr[i]; */
+    /*         else dp[i][j] = max(arr[i] - dp[i+1][j], arr[j] - dp[i][j-1]); */
+    /*     } */
+    /* } */
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
             if(i == j) dp[i][j] = arr[i];
-            else dp[i][j] = max(arr[i] - dp[i+1][j], arr[j] - dp[i][j-1]);
+            else if(i+1 == j) dp[i][j] = max(arr[i], arr[j]);
+            else if(i + 2 < n && j - 2 >= 0) dp[i][j] = max(arr[i]+min(dp[i+2][j], dp[i+1][j-1]), arr[j]+min(dp[i][j-2],dp[i+1][j-1]));
         }
     }
 
-    cout << (sum + dp[0][n-1]) / 2LL << '\n';
+    cout << dp[n-1][n-1];
 	return 0;
 }
